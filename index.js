5ea5e7f99f53295e07ec8a1d2622efb19dc5fcb9
Tensor <46503752+TensileTensor@users.noreply.github.com>
2022-08-01 18:58:17 -0700

    Fixed Connections Section

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ burgerIcon.addEventListener('click', () => {
 ****************************************************************************************************/
 
 /****************************************************************************************************
-* Practices Menu
+* Meet the Team (Tabbed Content)
 ****************************************************************************************************/
 
-const tabs = document.querySelectorAll('.menu-list li');
-const tabContentBoxes = document.querySelectorAll('#practices-content > div');
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tab-content > div');
 
 //Iterate through every tab
 tabs.forEach((tab) => {
     //Listen for a click on a tab
     tab.addEventListener('click', () => {
         //If a click is registered, deactivate all tabs
-        tabs.forEach(item => item.classList.remove('has-background-light'));
+        tabs.forEach(item => item.classList.remove('is-active'));
         //Activate the tab that's been clicked
-        tab.classList.add('has-background-light');
+        tab.classList.add('is-active');
         //Swap out content depending on active tag
         const target = tab.dataset.target;
 
@@ -48,27 +48,29 @@ const indexFooter = document.querySelector(".main-footer")
 
     indexFooter.innerHTML = `
 
-    
-        <div class="columns is-centered">
-            <div class="column is-3">
+    <div class="columns is-variable is-centered">
+        
+            <div class="column center is-3">
                 <a href="index.html">
-                    <img src="Assets\\Logos\\CR\\Full\\White.png" style="margin:0; padding:0; width:250px;">
+                    <img src="Assets\\Logos\\NicheCloudware\\Full\\White.png" style="margin:0; padding:0; width:250px;">
                 </a>
             </div>
-        </div>
-        <div class="columns is-variable is-centered">
-
+        
             <div class="column is-3 is-responsive center">
-                    <p class="my-2 mx-6 has-text-weight-light" style="text-align: center;">PRACTICES</p>
-                    <p class="my-2 mx-6 has-text-weight-light" style="text-align: center;">ABOUT</p>
-                    <p class="my-2 mx-6 has-text-weight-light" style="text-align: center;">CULTURE</p>
-                    <p class="my-2 mx-6 has-text-weight-light" style="text-align: center;">CONTACT</p>
+                <div class="content">
+                    <p class="my-2 has-text-weight-light" style="text-align: center;"> 17201, NW Viola Street, Portland
+                        97229,
+                        Oregon</p>
+                    <p class="my-2 has-text-weight-light" style="text-align: center;">+1.310.237.5924</p>
+                    <p class="my-2 has-text-weight-light" style="text-align: center;">hr@nichecloudware.com</p>
+                    <p class="my-2 has-text-weight-light" style="text-align: center;">careers@nichecloudware.com</p>
+                </div>
             </div>
         </div>
         
         <hr class="m-0 mb-3 p-0" style="margin-left: 20% !important; width: 60%;">
         
-        <p class="has-text-weight-light center">&copy; 2022 COHEN ROTHSCHILDS PLLC</p>
+        <p class="has-text-weight-light center">&copy; 2022 Niche Cloudware</p>
 
 `;
 } catch (e) {
@@ -78,25 +80,28 @@ const indexFooter = document.querySelector(".main-footer")
 const footers = document.querySelectorAll(".other-page-footer");  
     footers[0].innerHTML = `
     
-        <div class="columns is-centered">
-            <div class="column is-3">
-                <a href="index.html">
-                    <img src="..\\Assets\\Logos\\CR\\Full\\White.png" style="display:block; margin:auto; padding:0; width:250px;">
+    <div class="columns is-variable is-centered">
+
+            <div class="column center is-3">
+                <a href="../index.html">
+                    <img src="..\\Assets\\Logos\\NicheCloudware\\Full\\White.png" style="margin:0; padding:0; width:250px;">
                 </a>
+            </div>
+
+            <div class="column is-3 is-responsive center">
+                <div class="content">
+                    <p class="my-2 has-text-weight-light" style="text-align: center;"> 17201, NW Viola Street, Portland
+                        97229,
+                        Oregon</p>
+                    <p class="my-2 has-text-weight-light" style="text-align: center;">+1.310.237.5924</p>
+                    <p class="my-2 has-text-weight-light" style="text-align: center;">hr@nichecloudware.com</p>
+                    <p class="my-2 has-text-weight-light" style="text-align: center;">careers@nichecloudware.com</p>
+                </div>
             </div>
         </div>
 
-        <div class="columns is-variable is-centered">
-            <div class="column is-3 is-responsive center">
-                    <p class="my-2 mx-6 has-text-weight-light" style="text-align: center;">PRACTICES</p>
-                    <p class="my-2 mx-6 has-text-weight-light" style="text-align: center;">ABOUT</p>
-                    <p class="my-2 mx-6 has-text-weight-light" style="text-align: center;">CULTURE</p>
-                    <p class="my-2 mx-6 has-text-weight-light" style="text-align: center;">CONTACT</p>
-            </div>
-        </div>
-            
         <hr class="m-0 mb-3 p-0" style="margin-left: 20% !important; width: 60%;">
-        
-        <p class="has-text-weight-light center">&copy; 2022 COHEN ROTHSCHILDS PLLC</p>
+
+        <p class="has-text-weight-light center">&copy; 2022 Niche Cloudware</p>
     
     `;
