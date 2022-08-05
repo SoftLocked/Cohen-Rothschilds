88e85b0d39633c7cb0a47124a56e39489dc17d6b
Tensor <46503752+TensileTensor@users.noreply.github.com>
2022-08-04 18:03:50 -0700

    Draft of CR Website

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ burgerIcon.addEventListener('click', () => {
 ****************************************************************************************************/
 
 /****************************************************************************************************
-* Meet the Team (Tabbed Content)
+* Practices Menu
 ****************************************************************************************************/
 
-const tabs = document.querySelectorAll('.tabs li');
-const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+const tabs = document.querySelectorAll('.menu-list li');
+const tabContentBoxes = document.querySelectorAll('#practices-content > div');
 
 //Iterate through every tab
 tabs.forEach((tab) => {
     //Listen for a click on a tab
     tab.addEventListener('click', () => {
         //If a click is registered, deactivate all tabs
-        tabs.forEach(item => item.classList.remove('is-active'));
+        tabs.forEach(item => item.classList.remove('has-background-light'));
         //Activate the tab that's been clicked
-        tab.classList.add('is-active');
+        tab.classList.add('has-background-light');
         //Swap out content depending on active tag
         const target = tab.dataset.target;
 
@@ -39,6 +39,22 @@ tabs.forEach((tab) => {
 });
 
 /****************************************************************************************************
+* Culture Word Cycle
+****************************************************************************************************/
+
+const culturalValues = document.querySelector('#cultural-values');
+
+let values = ['COLLABORATIVE', 'INCLUSIVE', 'CLIENT-CENTRIC', 'LASER-FOCUSED'];
+
+let cultureIter = 1;
+
+setInterval(function () {
+    culturalValues.innerHTML = values[cultureIter%4];
+    cultureIter++;
+}, 1500);
+
+
+/****************************************************************************************************
 * Dynamic Footer
 ****************************************************************************************************/
 
@@ -48,29 +64,32 @@ const indexFooter = document.querySelector(".main-footer")
 
     indexFooter.innerHTML = `
 
-    <div class="columns is-variable is-centered">
-        
-            <div class="column center is-3">
+    
+        <div class="columns is-centered">
+            <div class="column is-3">
                 <a href="index.html">
-                    <img src="Assets\\Logos\\NicheCloudware\\Full\\White.png" style="margin:0; padding:0; width:250px;">
+                    <img src="Assets\\Logos\\CR\\Full\\White.png" style="display:block; margin:auto; padding:0; width:250px;">
                 </a>
             </div>
-        
-            <div class="column is-3 is-responsive center">
-                <div class="content">
-                    <p class="my-2 has-text-weight-light" style="text-align: center;"> 17201, NW Viola Street, Portland
-                        97229,
-                        Oregon</p>
-                    <p class="my-2 has-text-weight-light" style="text-align: center;">+1.310.237.5924</p>
-                    <p class="my-2 has-text-weight-light" style="text-align: center;">hr@nichecloudware.com</p>
-                    <p class="my-2 has-text-weight-light" style="text-align: center;">careers@nichecloudware.com</p>
-                </div>
+        </div>
+        <div class="columns is-variable is-centered">
+            <div class="column is-1-desktop is-2-tablet center">
+                <p class="mx-6 has-text-weight-light" style="text-align: center;">PRACTICES</p>
+            </div>
+            <div class="column is-1-desktop is-2-tablet center">
+                <p class="mx-6 has-text-weight-light" style="text-align: center;">ABOUT</p>
+            </div>
+            <div class="column is-1-desktop is-2-tablet center">
+                <p class="mx-6 has-text-weight-light" style="text-align: center;">CULTURE</p>
+            </div>
+            <div class="column is-1-desktop is-2-tablet center">
+                <p class="mx-6 has-text-weight-light" style="text-align: center;">CONTACT</p>
             </div>
         </div>
         
         <hr class="m-0 mb-3 p-0" style="margin-left: 20% !important; width: 60%;">
         
-        <p class="has-text-weight-light center">&copy; 2022 Niche Cloudware</p>
+        <p class="has-text-weight-light center">&copy; 2022 COHEN ROTHSCHILDS PLLC</p>
 
 `;
 } catch (e) {
@@ -80,28 +99,40 @@ const indexFooter = document.querySelector(".main-footer")
 const footers = document.querySelectorAll(".other-page-footer");  
     footers[0].innerHTML = `
     
-    <div class="columns is-variable is-centered">
-
-            <div class="column center is-3">
-                <a href="../index.html">
-                    <img src="..\\Assets\\Logos\\NicheCloudware\\Full\\White.png" style="margin:0; padding:0; width:250px;">
+        <div class="columns is-centered">
+            <div class="column is-3">
+                <a href="index.html">
+                    <img src="..\\Assets\\Logos\\CR\\Full\\White.png" style="display:block; margin:auto; padding:0; width:250px;">
                 </a>
-            </div>
-
-            <div class="column is-3 is-responsive center">
-                <div class="content">
-                    <p class="my-2 has-text-weight-light" style="text-align: center;"> 17201, NW Viola Street, Portland
-                        97229,
-                        Oregon</p>
-                    <p class="my-2 has-text-weight-light" style="text-align: center;">+1.310.237.5924</p>
-                    <p class="my-2 has-text-weight-light" style="text-align: center;">hr@nichecloudware.com</p>
-                    <p class="my-2 has-text-weight-light" style="text-align: center;">careers@nichecloudware.com</p>
-                </div>
             </div>
         </div>
 
+        <div class="columns is-variable is-centered">
+            <div class="column is-1 center">
+                <p class="mx-6 has-text-weight-light" style="text-align: center;">PRACTICES</p>
+            </div>
+            <div class="column is-1 center">
+                <p class="mx-6 has-text-weight-light" style="text-align: center;">ABOUT</p>
+            </div>
+            <div class="column is-1 center">
+                <p class="mx-6 has-text-weight-light" style="text-align: center;">CULTURE</p>
+            </div>
+            <div class="column is-1 center">
+                <p class="mx-6 has-text-weight-light" style="text-align: center;">CONTACT</p>
+            </div>
+        </div>
+            
         <hr class="m-0 mb-3 p-0" style="margin-left: 20% !important; width: 60%;">
-
-        <p class="has-text-weight-light center">&copy; 2022 Niche Cloudware</p>
-    
+        
+        <div class="columns is-centered">
+            <div class="column is-2">
+                <p class="has-text-weight-light center" style="text-align: center;">(631) 636-1750</p>
+            </div>
+            <div class="column is-3">
+                <p class="has-text-weight-light center" style="text-align: center;">14555 Dallas Parkway Drive, suite 100, Dallas, TX, 75254</p>
+            </div>
+            <div class="column is-2">
+                <p class="has-text-weight-light center" style="text-align: center;">&copy; 2022 COHEN ROTHSCHILDS PLLC</p>
+            </div>
+        </div>
     `;
